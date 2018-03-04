2805974a7bce40bf6740753584b76bfeea4c9f2d
Ambyr-Shae Jarrell <arjarrel@uno.edu>
2018-03-03 20:32:32 -0600

    Set up project
    Added all necessary files to start the exercise.

---
+++ out-tsc/package-installer.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var PackageInstaller = (function () {
+    function PackageInstaller() {
+    }
+    return PackageInstaller;
+}());
+exports.PackageInstaller = PackageInstaller;
